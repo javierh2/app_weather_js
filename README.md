f48eaf1f95edbbfc28f63c6a0ef433484672657c
javier <javier.morabaiz@gmail.com>
2025-04-11 15:52:04 -0300

    initiation of script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # app_weather_js
+
+
+https://openweathermap.org/api de donde tomé la api
+consultas generadas a postman
+
+
+
+html css js
---
+++ js/script.js
@@ -0,0 +1,12 @@
+// variables con la api y su API key
+//  ?q={city name}&appid={API key}   formato del  query que hace la API
+const urlApi = `https://api.openweathermap.org/data/2.5/weather`
+const API_LEY = '0e63d23f09f186800236585ad8f539e8'
+
+// variable de diferencia de K° a C°
+const diffKelvin = 273.15
+
+// acción sobre el botón 'search button'
+document.getElementById('searchButton')
+
+
