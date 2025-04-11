124355c8c09cf4343c04a406ab5fc08a67464825
javier <javier.morabaiz@gmail.com>
2025-04-11 19:21:15 -0300

    script finished

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 
 https://openweathermap.org/api de donde tomé la api
-consultas generadas a postman
+https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+peticiones generadas a postman  (cliente)
+para poder hacer llamadsa y peticiones http y ver la info antes de aplicarla en la APP (web)
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,12 +1,59 @@
 // variables con la api y su API key
 //  ?q={city name}&appid={API key}   formato del  query que hace la API
 const urlApi = `https://api.openweathermap.org/data/2.5/weather`
-const API_LEY = '0e63d23f09f186800236585ad8f539e8'
+const API_KEY = '0e63d23f09f186800236585ad8f539e8'
 
 // variable de diferencia de K° a C°
 const diffKelvin = 273.15
 
 // acción sobre el botón 'search button'
-document.getElementById('searchButton')
+document.getElementById('searchButton').addEventListener('click', () =>{
+    const city = document.getElementById('cityInput').value  // variable con el input de la ciudad
+    if(city){
+        fetchWeather(city) // uso de la API y la información del clima
+    }else{
+        alert('Please choose a valid City')
+    }
+})
 
+// llamar datos del clima + query de la API
+function fetchWeather(city){
+    fetch(`${urlApi}?q=${city}&appid=${API_KEY}`)
+    .then(data => data.json())
+    // .then(data => console.log(data))  para mostrar en consola
+    .then(data => showWeather(data))
+}
 
+// manejo del json y la data, mediante Postman
+function showWeather(data){
+    const responseData = document.getElementById('responseData')
+    responseData.innerHTML = ''
+
+    const cityName = data.name
+    const countryName = data.sys.country
+    const temp = data.main.temp
+    const humidity = data.main.humidity
+    const description = data.weather[0].description
+    const iconWeather = data.weather[0].icon
+
+    const cityInfo = document.createElement('h2')
+    cityInfo.textContent = `${cityName}, ${countryName}`
+
+    const tempInfo = document.createElement('p')
+    tempInfo.textContent = `The temperature is: ${Math.floor(temp-diffKelvin)}°C`
+
+    const humidityInfo = document.createElement('p')
+    humidityInfo.textContent = `The humidity is: ${humidity}`
+
+    const showIcon = document.createElement('img')
+    showIcon.src = `https://openweathermap.org/img/wn/${iconWeather}@2x.png`
+
+    const showDescription = document.createElement('p')
+    showDescription.textContent = `The weather information is: ${description}`
+
+    responseData.appendChild(cityInfo)
+    responseData.appendChild(tempInfo)
+    responseData.appendChild(humidityInfo)
+    responseData.appendChild(showIcon)
+    responseData.appendChild(showDescription)
+}
